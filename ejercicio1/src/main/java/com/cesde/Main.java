b3463eadf0b4152c9450eed68edb9170c306fb30
Jhon Fredy <jfinfocesde@gmail.com>
2025-03-13 08:36:14 -0500

    Ejercicio 1 resuelto

--- ejercicio1/src/main/java/com/cesde/Main.java
+++ ejercicio1/src/main/java/com/cesde/Main.java
@@ -3,5 +3,8 @@ package com.cesde;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x = 10;
+        int y = 20;
+        System.out.println(x+y);
     }
 }
